2fcc1a76cda47bc0598a71bf8e6802f396b653d4
Carlos Peralta Acon <caropeac@gmail.com>
2015-07-11 15:53:48 -0600

    Initial Commit

---
+++ CENFO Movies/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CENFO Movies
+//
+//  Created by MAC on 7/11/15.
+//  Copyright (c) 2015 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CENFO Movies/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CENFO Movies
+//
+//  Created by MAC on 7/11/15.
+//  Copyright (c) 2015 MAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
